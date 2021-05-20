55f280b9e5e566ac581fbfdbf3853f2527ab1d91
SvenTigges <sven.tigges@web.de>
2021-05-20 09:08:28 +0200

    1. Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -2,3 +2,16 @@
 /***** Funktionen 01 *****/
 
 // 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body)
+function test()
+{    
+console.log("Hallo Marc!");
+}
+
+// Funktionsaufruf (call)
+test();
+
+
+//prompt()
+//parseFloat()
