78c4a3f4e541136a793fbc5619f53fb35d667226
SvenTigges <sven.tigges@web.de>
2021-05-20 10:14:32 +0200

    2a. Parametrisierung + Datenübergabe von Innen

--- script_01.js
+++ script_01.js
@@ -6,12 +6,21 @@
 // Funktionsrumpf (body)
 function test()
 {    
-console.log("Hallo Marc!");
+    console.log("Hallo Marc!");
 }
 
 // Funktionsaufruf (call)
-test();
+//test(); // call auserbetireb, die Funktion wird nicht abgearbeitet 
 
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von Innen / trennen der Dinge die sich ändern von den Dingen die sich nicht ändern 
 
-//prompt()
-//parseFloat()
+function ausgabeNamen() // refactoring = umschreiben vorhandener Code
+{ 
+    let firstName = "deniz"; // Variable --> lokal
+    console.log("Hallo " +  firstName  + "!");   
+}
+
+ausgabeNamen();
+
+//console.log(firstName); // Fehler --> lokal!!!
